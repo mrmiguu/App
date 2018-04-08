648f7c5880efe3c354376f1400ef0966c730cac7
PǝRV <alexander.w.matulionis@gmail.com>
2018-04-08 10:45:44 -0700

    Revamp underway

--- example/main.go
+++ example/main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"github.com/mrmiguu/App/my"
-	"github.com/mrmiguu/App/our"
-)
-
 func main() {
-	menu := my.New("menu")
-	menu.AddImage("myMenu.png")
 
-	world := our.New("world")
-	world.AddImage("ourMenu.jpg")
-
-	start.Show()
-
-	start.Keyboard = true
-
-	start.HitFunc(func() {
-		start.Hide()
-		login.Show()
-	})
-
-	login.HitFunc(func() {
-		login.Hide()
-	})
 }
